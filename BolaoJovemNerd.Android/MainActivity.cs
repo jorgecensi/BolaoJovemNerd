f2ffb35c5dbbb68364701cf41afd4ae76dfb398d
Jorge Censi <jorgecensi@gmail.com>
2019-04-20 15:40:00 +1000

    app icon changed

--- BolaoJovemNerd.Android/MainActivity.cs
+++ BolaoJovemNerd.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace BolaoJovemNerd.Droid
 {
-    [Activity(Label = "BolaoJovemNerd", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "BolaoJovemNerd", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
