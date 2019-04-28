214e3c627f6c277ce3c851e3726fb1b43ac83f94
Jorge Censi <jorgecensi@gmail.com>
2019-04-28 20:00:02 +1000

    Difference field to the app

--- BolaoJovemNerd/Services/MockDataStore.cs
+++ BolaoJovemNerd/Services/MockDataStore.cs
@@ -15,12 +15,30 @@ namespace BolaoJovemNerd.Services
             items = new List<Item>();
             var mockItems = new List<Item>
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Jovem Nerd", Description="19", CharactersList="Varys, Jon Snow,Briene, Cão, Euron, Cersei, Jaime ", Avatar="jovemnerd.jpeg", Position ="1" },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "JP", Description="13", CharactersList="", Avatar="JP.jpeg", Position ="2" },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Marcelo", Description="12", CharactersList="", Avatar="marcelo.jpg", Position ="3" },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Tucano", Description="11", CharactersList="", Avatar="tucano.jpg", Position ="4" },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Katiucha", Description="10", CharactersList="", Avatar="Katiucha.jpg", Position ="5" },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Azaghal", Description="9", CharactersList="", Avatar="azaghal.jpg", Position ="6" }
+                new Item {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "Jovem Nerd",
+                    Description="19",
+                    CharactersList="Varys, Jon Snow,Briene, Cão, Euron, Cersei, Jaime ",
+                    Avatar="jovemnerd.jpeg",
+                    Position ="1",
+                    Difference = "+1" },
+                new Item {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = "JP",
+                    Description="13",
+                    CharactersList="",
+                    Avatar="JP.jpeg",
+                    Position ="2",
+                    Difference = "-1" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Marcelo", Description="12", CharactersList="", Avatar="marcelo.jpg", Position ="3",
+                    Difference = "-1" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Tucano", Description="11", CharactersList="", Avatar="tucano.jpg", Position ="4",
+                    Difference = "-1" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Katiucha", Description="10", CharactersList="", Avatar="Katiucha.jpg", Position ="5",
+                    Difference = "-1" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Azaghal", Description="9", CharactersList="", Avatar="azaghal.jpg", Position ="6",
+                    Difference = "-1" }
             };
 
             foreach (var item in mockItems)
